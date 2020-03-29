cbbee472f949f0c95954a6b4114e7894ec394de0
reezafa <a.fakhreeza@gmail.com>
2020-03-29 11:23:24 +0800

    last update by me

--- InputOutput.java
+++ InputOutput.java
@@ -4,9 +4,23 @@ public class InputOutput{
 	public static void main (String [] args){
 		Scanner scanner = new Scanner(System.in);
 		String nama;
+		int jumlahSks;
+		double IPK;
+		
+		
 		System.out.print("Input your name : ");
 		nama = scanner.nextLine();
 		System.out.println("Harooo, "+nama);
+		
+		System.out.print("Input accumulated SKS you've already taken: ");
+		jumlahSks = scanner.nextInt();
+		
+		System.out.print("Input temporary IPK: ");
+		IPK = scanner.nextDouble();
+		
+		double jumlah = IPK * jumlahSks;
+		System.out.println("Total Value = "+jumlah);
+		
 		
 	}
 }
