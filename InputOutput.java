31813ada4629bdee366154fea19648e4379dfcc6
reezafa <a.fakhreeza@gmail.com>
2020-03-29 11:11:26 +0800

    Compile & Run Program

--- InputOutput.java
+++ InputOutput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputOutput{
-	publi static void main (String [] args){
+	public static void main (String [] args){
 		Scanner scanner = new Scanner(System.in);
 		String nama;
 		System.out.print("Input your name : ");
