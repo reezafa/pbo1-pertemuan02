39a79adf97ea49997a41de22bd88c1704ed0021c
reezafa <a.fakhreeza@gmail.com>
2020-03-29 11:07:44 +0800

    String Input kedalam Variable

--- InputOutput.java
+++ InputOutput.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class InputOutput{
 	publi static void main (String [] args){
 		Scanner scanner = new Scanner(System.in);
+		String nama;
+		System.out.print("Input your name : ");
+		nama = scanner.nextLine();
 		
 	}
 }
